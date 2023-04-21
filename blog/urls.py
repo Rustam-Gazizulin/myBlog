e2ea68738254d67b7fe4888d780ff3a232e55d2c
Rustam-Gazizulin <rgazizulin174@gmail.com>
2023-04-21 21:24:19 +0500

    разбивка урл на год-месяц-день-слаг

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = 'blog'
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
