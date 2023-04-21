9b55033f78bb635d5fe87d38e27c260b82024ea3
Rustam-Gazizulin <rgazizulin174@gmail.com>
2023-04-21 21:47:33 +0500

    Настроил пагинацию

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
