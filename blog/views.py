f0930f326d471467f692abb7c206895e1beca2b2
Rustam-Gazizulin <rgazizulin174@gmail.com>
2023-04-23 23:49:54 +0500

    Добавил возможность делиться постами оп email

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from .models import Post
-# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
 
 
-# def post_list(request):
-#     posts_list = Post.published.all()
-#     paginator = Paginator(posts_list, 3)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     return render(request, 'blog/post/list.html', {'posts': posts})
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} предлагает прочитать пост {post.title}"
+            message = f"Крутой пост {post.title} вот ссылка {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'rgazizulin174@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
--- myBlog/settings.py
+++ myBlog/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Конфигурация сервера электронной почты
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'rgazizulin174@gmail.com'
+EMAIL_HOST_PASSWORD = 'kgjkwtxcmhthdzxm'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
