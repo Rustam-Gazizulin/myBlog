e37e67ac1165132639e9f0a6fc6f0e3c755853a6
Rustam-Gazizulin <rgazizulin174@gmail.com>
2023-05-03 14:37:51 +0500

    Подключил PostgresQL перенес данные из базы SQL lite

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "myBlog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "blog",
+        "USER": "blog",
+        "PASSWORD": "redzone174",
+        "PORT": "5432",
+        "HOST": "localhost",
     }
 }
 
