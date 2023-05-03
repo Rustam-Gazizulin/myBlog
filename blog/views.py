6ad8c78bb549c3ee40db01c7af15e3681c021042
Rustam-Gazizulin <rgazizulin174@gmail.com>
2023-05-03 19:50:11 +0500

    Добавил интелектуальный поиск который ищет слова с ошибками

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from .models import Post
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 
 def post_share(request, post_id):
@@ -114,9 +114,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            # search_vector = SearchVector('title', 'body')
+            # search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
 
     return render(request,
                   'blog/post/search.html',
