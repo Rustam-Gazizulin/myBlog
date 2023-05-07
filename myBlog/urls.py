58badc8f4fe1fc651c1037062dcb82af61314689
Rustam-Gazizulin <rgazizulin174@gmail.com>
2023-05-07 15:26:36 +0500

    Настроил CI CD FIX12

--- myBlog/urls.py
+++ myBlog/urls.py
@@ -10,6 +10,6 @@ sitemaps = {
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("blog/", include('blog.urls', namespace='blog')),
+    path("", include('blog.urls', namespace='blog')),
     path("sitemap.xml", sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
